1ef95478a9b44e8c8862ca70e87b717e7e1584dc
xyzvandung <x.yzvandung@gmail.com>
2026-01-09 17:39:40 +0700

    Update Rayfield URL and GUI loading texts

--- aimbot2.lua
+++ aimbot2.lua
@@ -1,4 +1,4 @@
-local Rayfield = loadstring(game:HttpGet('https://tinyurl.com/27o6fswj'))()
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
@@ -61,10 +61,10 @@ RunService.RenderStepped:Connect(function()
 end)
 
 -- Rayfield GUI
-local Window = Rayfield:CreateWindow({
+local Window = xyzvandung:CreateWindow({
 	Name = "Aimbot FOV",
-	LoadingTitle = "Cargando...",
-	LoadingSubtitle = "By _zxmisaxz_",
+	LoadingTitle = "Loading...",
+	LoadingSubtitle = "xyzvandung",
 	ConfigurationSaving = {
 		Enabled = true,
 		FolderName = "AimbotFOV",
